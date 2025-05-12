7aa57b912bb727624b1856503e490f43a1b33368
bthrbrbrbrbfbtbtbt <rakeshmeena0099887766@gmail.com>
2025-05-12 08:46:42 +0530

    Update sahil.py

--- sahil.py
+++ sahil.py
@@ -11,12 +11,12 @@ from threading import Thread
 loop = asyncio.get_event_loop()
 
 # Bot token
-TOKEN = '7362289727:AAE6RWfho65aJUxMyOSdvxXVBndKGx4QZcc'
+TOKEN = '7595588536:AAFAEtz2b_dd_64KkCehbwVZwH0fkPM5DNk'
 bot = telebot.TeleBot(TOKEN)
 REQUEST_INTERVAL = 1
 
 # Admins list
-ADMIN_IDS = [2085082046]  # Replace with actual admin user IDs
+ADMIN_IDS = [6769245930]  # Replace with actual admin user IDs
 
 # File to store user information
 USERS_FILE = 'users.txt'
